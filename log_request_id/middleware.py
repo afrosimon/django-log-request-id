b34f0359f601a0b1c5fc71073bd0af612648e8d9
Simon Chenard <chenard.simon@gmail.com>
2016-11-23 12:01:20 +0100

    too much logging...

--- log_request_id/middleware.py
+++ log_request_id/middleware.py
@@ -19,7 +19,6 @@ class RequestIDMiddleware(MiddlewareMixin):
         request_id = self._get_request_id(request)
         local.request_id = request_id
         request.id = request_id
-        logger.info('[RequestIDMiddleware] Starting to log for request ID {}'.format(request_id))
 
     def process_response(self, request, response):
         if getattr(settings, REQUEST_ID_RESPONSE_HEADER_SETTING, False) and getattr(request, 'id', None):
